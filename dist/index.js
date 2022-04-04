776571e68a285c86d392254f93feebf7ff748e67
jonathan.arias <jonathan.arias@lottuseducation.com>
2022-04-04 00:13:06 -0500

    Carga inicial

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.holaAlCursoNPM = exports.holaPersonalizado = exports.hola = void 0;
+function hola() {
+    console.log('Hola Mundo!!!');
+}
+exports.hola = hola;
+function holaPersonalizado(nombre) {
+    console.log(`Hola ${nombre}`);
+}
+exports.holaPersonalizado = holaPersonalizado;
+function holaAlCursoNPM() {
+    console.log('Hola al curso NPM');
+}
+exports.holaAlCursoNPM = holaAlCursoNPM;
